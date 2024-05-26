9f72aa2da436045c06ba8cfe639d95b88c2e66d2
Ataullah Mesbah Fahim <mesbahataullah4@gmail.comm>
2024-05-27 03:22:02 +0600

    fb and youtube up

--- src/app/components/YouTubeEmbed/YouTubeEmbed.js
+++ src/app/components/YouTubeEmbed/YouTubeEmbed.js
@@ -1,16 +1,16 @@
-'use client'
 
+'use client'
 import React, { useState } from 'react';
 
-const YouTubeEmbed = ({ videoIds }) => {
+const YouTubeEmbed = ({ videoIds, facebookVideoUrls }) => {
     const [selectedVideo, setSelectedVideo] = useState(null);
 
     return (
         <div className="container mx-auto px-4 lg:px-16 py-8 mt-10">
-
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 lg:gap-6 ">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 lg:gap-6">
+                {/* Render YouTube videos */}
                 {videoIds.map((id) => (
-                    <div key={id} className="cursor-pointer">
+                    <div key={id} className="video-item cursor-pointer">
                         <iframe
                             className="w-full h-52 sm:h-56 md:h-64 rounded-md"
                             src={`https://www.youtube.com/embed/${id}`}
@@ -20,6 +20,23 @@ const YouTubeEmbed = ({ videoIds }) => {
                         ></iframe>
                     </div>
                 ))}
+                {/* Render Facebook videos */}
+                {facebookVideoUrls.map((url) => (
+                    <div key={url} className="video-item cursor-pointer">
+                        <iframe
+                            className="w-full h-52 sm:h-56 md:h-64 rounded-md" // Adjust the height to match YouTube videos
+                            src={`https://www.facebook.com/plugins/video.php?href=${url}&show_text=0&width=560`}
+                            width="560"
+                            height="315"
+                            style={{ border: "none", overflow: "hidden" }}
+                            scrolling="no"
+                            frameBorder="0"
+                            allowFullScreen="true"
+                            allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+                        ></iframe>
+                    </div>
+                ))}
+                {/* Render selected video */}
                 {selectedVideo && (
                     <div className="col-span-3 sm:col-span-2 lg:col-span-1 flex justify-center mt-4">
                         <iframe
@@ -34,10 +51,7 @@ const YouTubeEmbed = ({ videoIds }) => {
                     </div>
                 )}
             </div>
-
         </div>
-
-
     );
 };
 
--- src/app/components/videoGallery/videogallery.js
+++ src/app/components/videoGallery/videogallery.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import YouTubeEmbed from '../YouTubeEmbed/YouTubeEmbed';
 
-
 const videoGallery = () => {
     const videoIds = [
         'i1Ms7lmRD3c?si=OnZ0Tz18AkNz2p0I',
@@ -9,9 +8,16 @@ const videoGallery = () => {
         'wFn-jqLc10k?si=93liFfdFnBE3rb4U',
     ];
 
+    const facebookVideoUrls = [
+        'https://www.facebook.com/100023260389958/videos/1378739092700870/',
+        'https://www.facebook.com/reel/6987489514710138',
+        'https://fb.watch/sjojtwPPzL/',
+        // Add more Facebook video URLs as needed
+    ];
+
     return (
         <div className="p-4">
-            <YouTubeEmbed videoIds={videoIds} />
+            <YouTubeEmbed videoIds={videoIds} facebookVideoUrls={facebookVideoUrls} />
         </div>
     );
 };
