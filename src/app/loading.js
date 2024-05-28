f1ca99f447aafe6d7738c340f1164020c03afa6b
Ataullah Mesbah Fahim <mesbahataullah4@gmail.comm>
2024-05-29 00:23:16 +0600

    loader use

---
+++ src/app/loading.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const loading = () => {
+    return (
+        <div className="flex items-center justify-center min-h-screen bg-gray-100 fixed inset-0 z-50">
+            <div className="loader ease-linear rounded-full border-8 border-t-8 border-gray-200 h-32 w-32"></div>
+        </div>
+    );
+};
+
+export default loading;
